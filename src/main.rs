5f1dd028e730acc9ab3ae929fc680648b024a108
Vitalii Shishorin <moskrc@gmail.com>
2024-01-20 20:32:00 +0300

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,99 @@
-use quick_xml::de::Deserializer;
-use quick_xml::events::{BytesStart, Event};
-use quick_xml::reader::Reader;
-use quick_xml::writer::Writer;
-use reqwest::blocking::Client;
+use reqwest::blocking::{get, Response};
+use quick_xml::de::from_str;
 use serde::Deserialize;
-use std::io::BufRead;
+use encoding_rs::WINDOWS_1251;
+use std::cmp::Ord;
 
-#[derive(Deserialize, Debug)]
-struct Currency {
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct ValCurs {
+    #[serde(rename = "@Date")]
+    date: String,
+    #[serde(rename = "@name")]
+    name: String,
+    #[serde(rename = "Valute")]
+    valutes: Vec<Valute>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize, Eq)]
+struct Valute {
+    #[serde(rename = "@ID")]
+    id: String,
+    #[serde(rename = "NumCode")]
+    num_code: String,
     #[serde(rename = "CharCode")]
     char_code: String,
     #[serde(rename = "Nominal")]
-    nominal: i16,
+    nominal: u32,
     #[serde(rename = "Name")]
     name: String,
     #[serde(rename = "Value")]
     value: String,
+    #[serde(rename = "VunitRate")]
+    vunit_rate: String,
 }
 
-fn process_currency(currency: &Currency) {
-    println!(
-        "{}: {:>8} - {} ({})",
-        currency.char_code, currency.value, currency.name, currency.nominal
-    );
+impl ValCurs {
+    fn report(&self) {
+        println!("{:=^79}", " REPORT ");
+        println!("Date: {}", self.date);
+        println!("Name: {}", self.name);
+        println!("{:=^79}", "");
+        for i in 0..self.valutes.len() {
+            self.valutes[i].report();
+        }    
+    }
 }
 
-fn get_content(url: String) -> Result<String, reqwest::Error> {
-    let client: Client = Client::new();
-    let result: reqwest::blocking::Response = client.get(url).send()?;
-    Ok(result.text()?)
+impl Valute {
+    fn report(&self) {
+        println!("- {}", self.name);
+        
+        println!("Id: {}", self.id);
+        println!("NumCode: {}", self.num_code);
+        println!("CharCode: {}", self.char_code);
+        println!("Nominal: {}", self.nominal);
+        println!("Value: {}", self.value);
+        println!("UnitRate: {}", self.vunit_rate);
+        
+
+    }
+}
+    
+
+impl Ord for Valute {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.char_code.cmp(&other.char_code)
+    }
 }
 
-fn read_to_end_into_buffer<R: BufRead>(
-    reader: &mut Reader<R>,
-    start_tag: &BytesStart,
-    junk_buf: &mut Vec<u8>,
-) -> Result<Vec<u8>, quick_xml::Error> {
-    let mut depth: i32 = 0;
-    let mut output_buf: Vec<u8> = Vec::new();
-    let mut w: Writer<&mut Vec<u8>> = Writer::new(&mut output_buf);
-    let tag_name: quick_xml::name::QName<'_> = start_tag.name();
-    w.write_event(Event::Start(start_tag.clone()))?;
-    loop {
-        junk_buf.clear();
-        let event: Event<'_> = reader.read_event_into(junk_buf)?;
-        w.write_event(&event)?;
+impl PartialOrd for Valute {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
-        match event {
-            Event::Start(e) if e.name() == tag_name => depth += 1,
-            Event::End(e) if e.name() == tag_name => {
-                if depth == 0 {
-                    return Ok(output_buf);
-                }
-                depth -= 1;
-            }
-            Event::Eof => {
-                panic!("oh no")
-            }
-            _ => {}
-        }
+impl PartialEq for Valute {
+    fn eq(&self, other: &Self) -> bool {
+        self.char_code == other.char_code
     }
 }
 
 fn main() {
-    let url: String = "https://www.cbr.ru/scripts/XML_daily.asp".to_string();
-    let content: String = get_content(url).unwrap();
+    let url: &str = "https://www.cbr.ru/scripts/XML_daily.asp"; // Replace with the actual URL
+    let response: Response = get(url).expect("Failed to fetch XML from the internet");
+    let bytes= response.bytes().unwrap();
+    let xml_string: String = decode_windows1251(&bytes);
+    println!("{:?}", xml_string);
 
-    let mut reader: Reader<&[u8]> = Reader::from_str(&content);
-    let mut buf: Vec<u8> = Vec::new();
-
-    let mut junk_buf: Vec<u8> = Vec::new();
-
-    loop {
-        match reader.read_event_into(&mut buf) {
-            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
-            Ok(Event::Eof) => break,
-            Ok(Event::Start(e)) => match e.name().as_ref() {
-                b"Valute" => {
-                    let release_bytes: Vec<u8> =
-                        read_to_end_into_buffer(&mut reader, &e, &mut junk_buf).unwrap();
-                    let str: &str = std::str::from_utf8(&release_bytes).unwrap();
-                    let mut deserializer: Deserializer<'_, quick_xml::de::SliceReader<'_>> = Deserializer::from_str(str);
-                    let currency: Currency = Currency::deserialize(&mut deserializer).unwrap();
-                    process_currency(&currency);
-                }
-                _ => (),
-            },
-            // other unimportant Events
-            _ => (),
-        }
-        // clear buffer to avoid leaking memory
-        buf.clear();
-    }
-
+    let mut valcurs: ValCurs = from_str(&xml_string).expect("Failed to parse XML");
+    valcurs.valutes.sort();
+    valcurs.report()
 }
+
+fn decode_windows1251(bytes: &[u8]) -> String {
+    let (decoded, _, _) = WINDOWS_1251.decode(bytes);
+    decoded.into_owned()
+}
+
